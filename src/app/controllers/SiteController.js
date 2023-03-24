0f34719f871b39010e36842ec8b4a4ccfc48ff93
duyphuc174 <nguyenduyphuc.174@gmail.com>
2023-03-24 10:08:50 +0700

    học nodejs

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,9 +1,20 @@
 
+const Course = require('../models/Course');
+const { mutipleMongooseToObject } = require('../../util/mongoose');
+
 class NewsController {
 
     // [GET] /
-    index(req, res) {
-        res.render('home');
+    index(req, res, next) {
+        
+        Course.find({})
+            .then(courses => {
+                res.render('home', {
+                    courses: mutipleMongooseToObject(courses)
+                });
+            })
+            .catch(next);
+
     };
 
     // [GET] /search
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
+const methodOverride = require('method-override');
 const { engine } = require('express-handlebars');
-const app = express();
-const port = 3000;
 
 const route = require('./routes');
+const db = require('./config/db');
+
+// Connect to db
+db.connect();
+
+const app = express();
+const port = 3002;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -14,18 +20,23 @@ app.use(express.urlencoded({
 }));
 app.use(express.json());
 
+app.use(methodOverride('_method'));
+
 // HTTP logger
-app.use(morgan('combined'));
+//app.use(morgan('combined'));
 
 // Template engine
 app.engine(
     'hbs',
     engine({
         extname: '.hbs',
+        helpers: {
+            sum: (a, b) => a + b,
+        }
     }),
 );
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
 // Routes init
 route(app);
